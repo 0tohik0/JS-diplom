786e1252d3a3145f6d830d33a23097306e6182fb
0tohik0 <gryaznowa.katyu@yandex.ru>
2022-05-09 16:08:13 +0300

    add webpack

---
+++ src/modules/one.js
@@ -0,0 +1,7 @@
+const oneModule = () => {
+    const one = "Первый рабочий скрипт"
+
+    console.log(one);
+}
+
+export default oneModule
---
+++ src/modules/two.js
@@ -0,0 +1,7 @@
+const twoModule = () => {
+    const two = "Второй рабочий скрипт"
+
+    console.log(two);
+}
+
+export default twoModule
