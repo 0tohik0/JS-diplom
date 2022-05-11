65dc29ef3a60862e18e2ab0bd11c34e6dcfaad43
0tohik0 <gryaznowa.katyu@yandex.ru>
2022-05-11 03:07:55 +0300

    add sendingModal and validateForm

--- src/index.js
+++ src/index.js
@@ -4,11 +4,32 @@ import timer from './modules/timer'
 import calculator from './modules/calculator'
 import modalPhoto from './modules/modalPhoto'
 import modalServices from './modules/modalServices'
-
+import sendingForm from './modules/sendingForm'
+import validateForm from './modules/validateForm'
 
 modal()
 scroll()
-timer('17 may 2022')
+timer('25 may 2022')
 calculator()
 modalPhoto()
 modalServices()
+sendingForm()
+validateForm()
+
+const forms = document.querySelectorAll('form')
+forms.forEach((form, index) => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        form.id = `form${index}`
+        
+        sendingForm({
+            formId: `form${index}`,
+            someElem: [
+                {
+                    type: 'block',
+                    id: 'calc-total'
+                }
+            ] 
+        })
+    })
+})
